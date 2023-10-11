fccdfad1f8445007b673ce957873aa7948566ed6
nickwatter <128505815+nickwatter@users.noreply.github.com>
2023-10-11 14:35:07 +0200

    sidebar

--- JS/sidenav.js
+++ JS/sidenav.js
@@ -16,12 +16,16 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     
-    document.body.style.backgroundColor = "rgb(0,0,0,0.2)";
+   // document.getElementById("overlay").classList.add("active");
+
+    // document.body.style.backgroundColor = "rgb(0,0,0,0.2)";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "110px";
     document.getElementById("main").style.marginLeft = "110px";
-    document.body.style.backgroundColor = "white";
+    // document.getElementById("overlay").classList.remove("active");
+
+    // document.body.style.backgroundColor = "white";
   }
