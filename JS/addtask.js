f4c2c6561e7d7d9a94add8d4a85dcef5312c3814
nickwatter <128505815+nickwatter@users.noreply.github.com>
2023-11-06 14:32:20 +0100

    up

--- JS/addtask.js
+++ JS/addtask.js
@@ -4,5 +4,6 @@ var modal = document.getElementById('id01');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        background.style.opacity = "0.5";
     }
 }
