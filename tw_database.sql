d4132554045a2c00cdfbb9ecae09c118310d12ec
sh0gun425 <joel.monza@stud.fhgr.ch>
2023-11-08 11:39:00 +0100

    Update tw_database.sql

--- tw_database.sql
+++ tw_database.sql
@@ -1,4 +1,8 @@
 -- Create the user table
 CREATE TABLE user (
-    
-)
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    nationality VARCHAR(50)
+);
